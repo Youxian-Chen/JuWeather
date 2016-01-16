839bc8a95a8caf578b5434410fdfe09b717d683a
Youxian Chen <youxian80925@gmail.com>
2016-01-16 14:50:24 +0800

    add Constant class

---
+++ app/src/main/java/com/example/youxian/juweather/Constant/Constant.java
@@ -0,0 +1,11 @@
+package com.example.youxian.juweather.Constant;
+
+/**
+ * Created by Youxian on 1/16/16.
+ */
+public class Constant {
+    public static final String LAST_LOCATION = "last_location";
+    public static final String LAST_LOCATION_LAT = "last_location_lat";
+    public static final String LAST_LOCATION_LON = "last_location_lon";
+    public static final String LAST_LOCATION_CITY = "last_location_city";
+}
